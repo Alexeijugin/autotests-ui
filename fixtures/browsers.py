bf2ae8bf82d537813f14c2cca90a083228b3a93a
Alexeijugin <57283269+Alexeijugin@users.noreply.github.com>
2025-12-30 19:41:48 +0300

    знакомство с PageObjec

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import Page, Playwright
 
+from pages.login_page import LoginPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,36 +1,20 @@
-from playwright.sync_api import sync_playwright, expect, Page
+from playwright.sync_api import Page
 import pytest
 
+from pages.login_page import LoginPage
+
 creds = {
-    ('user.name@gmail.com', 'password'): 'Invalid email and password',
-    ('user.name@gmail.com', ''): 'Invalid email, empty password',
-    ('', 'password'): 'Empty email, invalid password'
+    ("user.name@gmail.com", "password"): "Invalid email and password",
+    ("user.name@gmail.com", "  "): "Invalid email, empty password",
+    ("  ", "password"): "Empty email, invalid password",
 }
 
 
-@pytest.mark.parametrize("email, password",
-                         creds, ids=creds.values())
+@pytest.mark.parametrize("email, password", creds.keys(), ids=creds.values())
 @pytest.mark.regression  # Добавили маркировку regression
 @pytest.mark.authorization  # Добавили маркировку authorization
-def test_wrong_email_or_password_authorization(email: str, password: str, chromium_page: Page):
-    # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную.
-
-    # Переходим на страницу входа
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-    # Заполняем поле email
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    # Заполняем поле пароль
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    # Нажимаем на кнопку Login
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    # Проверяем, что появилось сообщение об ошибке
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+def test_wrong_email_or_password_authorization(email: str, password: str, login_page: LoginPage):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
